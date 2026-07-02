8b96c2d4228886328228bfa555e91f9b3d12c5e3
choijm25-cell <choijm25@ewha.ac.kr>
2026-07-02 05:11:42 +0000

    lab4

---
+++ decaration.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace Choijimin2593323
+{
+    struct studnet{
+        int ID{};
+        int score{};
+        
+    }
+   
+    int inputInteger();
+    int addInteger(int a, int b);
+
+}
--- main.cpp
+++ main.cpp
@@ -1,83 +1,33 @@
 #include <iostream>
+
+struct student
+{
+    int id{};
+    char grade{};
+};
+
+void printStudent(const student& s )
+{
+    std::cout << "ID:" << s.id << '\n';
+    std:: cout << "Grade:" << s.grade << '\n';
+}
+student inputStudent()
+{
+    student s{};
+    std:: cout << "ENter student ID:";
+    std:: cin >> s.id;
+    std::cout << "Eneter studnet garde (A/B/C/D/F)";
+    std:: cin >> s.grade;
+    return s;
+}
+
 int main()
 {
-    
-    int i{ 1 };
-    const int end{ 10 };
-    do
-{
-    std::cout << i << ' ';
-    ++i;
-} while (i <= end);
-    std::cout << std::endl;
-    i = 1;
-    while (i <= end)
-{
-    std::cout << i << ' ';
-    ++i;
-}
-std::cout << std::endl;
-// i = 1;
-// for ( ; i <= end; )
-// {
-// }
-// std::cout << i << ' ';
-// ++i;
-// std::cout << std::endl;
-// for (i = 1; i <= end; ++i)
-// {
-// std::cout << i << ' ';
-// }
-// std::cout << std::endl;
-// return 0;
-// }
-    // if (score >=90)
-    // grade = 'A';
-    // else if (score >= 80)
-    // grade = 'B';
-    // else if (score >= 70)
-    // grade = 'C';
-    // else if (score >= 60)
-    // grade = 'D';
-    // else
-    // grade = 'F';
-    // std::cout << "enter your score (0~100):";
-    // int score{};
-    // char grade{};
-    // std::cin >> score;
-    // switch (score/10)
-    // {
-    //     case 10:
-    //     case 9: grade = 'A'; break;
-    //     case 8: grade = 'B';break;
-    //     case 7: grade = 'C';break;
-    //     case 6: grade = 'D';break;
-    //     default: grade = 'F';
-    }
-    // std::cout << "Enter 1.2 or3: ";
-    // int x();
-    // std::cin >> x;
-    // switch(x)
-    // {
-    //     case 1: std::cout << "one\n"; break;
-    //     case 2: std:: cout << " two\n"; break;
-    //     case 3: std:: cout<< "three\n"; break;
-    //     default : std:: cout<< "unknown\n"; 
-    // }
-    // if (x>y)
-    // std::cout << "Enter an inteager:" ;
-    // int x{};
-    // std::cin >> x;
-    // if(x>0) std::cout << "positive\n";
-    // else if (x<0) std::cout << "neagtive\n";
-    // if (x==0) std::cout << "Zero\n";
-    // else std::cout << "nonZero\n";
+    student kim{ 1234567, 'A'};
+    printStudent(kim);
 
-    // int numOfStudents{30}; numOfStudents =20
-    // std::cout << numOfStudents << std:: endl;
-    // std::cout < sizeof(numOfStudents) << std:: endl;
-    // std::cout << typeid(numOfStudents).name () << std::endl;
-    // std::cout << typeid(static_cast<double>(numOfStudents)).name() << std::endl;
+    student lee{inputStudent()};
+    printStudent(lee);
 
-    // return 0;
+    return 0;
 }
